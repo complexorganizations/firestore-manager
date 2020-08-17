9b96dfd48d4d075b7a372f96b4ca57117290f9b3
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-08-17 08:45:00 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,200 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"github.com/urfave/cli/v2"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Commands = append(app.Commands,
+		&cli.Command{
+			Name:    "create",
+			Aliases: []string{"c"},
+			Flags: []cli.Flag{
+				CredentialsFile(),
+				Collection(),
+				Document(),
+				JsonFile(),
+			},
+			Usage: "create a document on firebase",
+			Action: func(c *cli.Context) error {
+				return Create(
+					c.String("credentials_file"),
+					c.String("collection"),
+					c.String("document"),
+					c.String("file"),
+				)
+			},
+		},
+		&cli.Command{
+			Name:    "set",
+			Aliases: []string{"s"},
+			Flags: []cli.Flag{
+				CredentialsFile(),
+				Collection(),
+				Document(),
+				JsonFile(),
+			},
+			Usage: "updates a document on firebase",
+			Action: func(c *cli.Context) error {
+				return Set(
+					c.String("credentials_file"),
+					c.String("collection"),
+					c.String("document"),
+					c.String("file"),
+				)
+			},
+		},
+		&cli.Command{
+			Name:    "delete",
+			Aliases: []string{"d"},
+			Flags: []cli.Flag{
+				CredentialsFile(),
+				Collection(),
+				Document(),
+			},
+			Usage: "deletes a document on firebase",
+			Action: func(c *cli.Context) error {
+				return Delete(
+					c.String("credentials_file"),
+					c.String("collection"),
+					c.String("document"),
+				)
+			},
+		})
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func Create(credentialsFile, collection, documentName, fileName string) error {
+	ctx := context.Background()
+	cli, err := New(ctx, credentialsFile)
+	if err != nil {
+		return err
+	}
+
+	content, err := ReadJsonFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	col := cli.Collection(collection)
+	cal := col.Doc(documentName)
+
+	_, err = cal.Create(ctx, content)
+	return err
+}
+
+func Delete(credentialsFile, collection, documentName string) error {
+	ctx := context.Background()
+	cli, err := New(ctx, credentialsFile)
+	if err != nil {
+		return err
+	}
+
+	col := cli.Collection(collection)
+	cal := col.Doc(documentName)
+
+	_, err = cal.Delete(ctx)
+	return err
+}
+
+func Set(credentialsFile, collection, documentName, file string) error {
+	ctx := context.Background()
+	cli, err := New(ctx, credentialsFile)
+	if err != nil {
+		return err
+	}
+
+	content, err := ReadJsonFile(file)
+	if err != nil {
+		return err
+	}
+
+	col := cli.Collection(collection)
+	cal := col.Doc(documentName)
+
+	_, err = cal.Set(ctx, content)
+	return err
+}
+
+func ReadJsonFile(file string) (map[string]interface{}, error) {
+	body, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	out := map[string]interface{}{}
+	err = json.Unmarshal(body, &out)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func New(ctx context.Context, file string) (*firestore.Client, error) {
+	opt := option.WithCredentialsFile(file)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func Collection() cli.Flag {
+	return &cli.StringFlag{
+		Name:     "collection",
+		Aliases:  []string{"c"},
+		Usage:    "--" + "collection" + " name",
+		EnvVars:  []string{"COLLECTION"},
+		Required: true,
+	}
+}
+
+func CredentialsFile() cli.Flag {
+	return &cli.StringFlag{
+		Name:     "authentication",
+		Aliases:  []string{"auth"},
+		Usage:    "--" + "authentication" + " name",
+		EnvVars:  []string{"AUTHENTICATION"},
+		Required: true,
+	}
+}
+
+func Document() cli.Flag {
+	return &cli.StringFlag{
+		Name:     "document",
+		Aliases:  []string{"d"},
+		Usage:    "--" + "document" + " name",
+		EnvVars:  []string{"DOCUMENT"},
+		Required: true,
+	}
+}
+
+func JsonFile() cli.Flag {
+	return &cli.StringFlag{
+		Name:     "file",
+		Aliases:  []string{"f"},
+		Usage:    "--" + "file" + " name",
+		Required: true,
+	}
+}
